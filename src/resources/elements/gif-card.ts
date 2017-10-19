f631762fdc3b2aecc0a41a02395dcbd12cccd17f
Michael A Manzano <michael.a.manzano@intel.com>
2017-10-19 13:17:54 -0700

    learning about dynamic requires

--- src/resources/elements/gif-card.ts
+++ src/resources/elements/gif-card.ts
@@ -12,4 +12,15 @@ export class GifCard {
   public srcChanged(o, n){
 
   }
+  public imagePath(src) {
+    console.log(src);
+   // return require(src);
+   let s = "success4.gif"
+    return require("../gifs/" + src);
+  }
 }
+
+//Solving the require problem
+//https://stackoverflow.com/questions/37241662/using-require-with-a-variable-vs-using-a-string-in-webpack/37241982
+//https://stackoverflow.com/questions/31694346/webpack-can-not-require-variable-the-request-of-a-dependency-is-an-expression/33048000#33048000
+//https://webpack.github.io/docs/context.html
