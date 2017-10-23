5b931eb23bcce96480708102af5fd7a220401814
mikezano <mikezano@gmail.com>
2017-10-23 00:08:21 -0700

    trying to base 64 store things

--- src/indexes/home.ts
+++ src/indexes/home.ts
@@ -7,20 +7,26 @@ import {GifEmbedDialog} from "../resources/elements/gif-embed-dialog";
 export class Home{
 
 	public gif:string;
-	public out:string;
+	public out:any;
+	public gifData: Blob;
+	public urlCreated: string;
 
     constructor(
         private apiService: GifApi,
-        private dialogService: DialogService
+		private dialogService: DialogService
     ){
     }
 
 	public attached(): void {
 
 		//setTimeout(() => { this.openDialog() }, 1000);
-		this.apiService.getAllGifs().then(result =>{
+		this.apiService.getAllGifs().then((result:any) =>{
 			console.log(result[0]);
 			this.out = result[0];
+			this.gifData = new Blob([this.out.data], {type: "image/png"});			
+			console.log(this.gifData);
+			this.urlCreated = URL.createObjectURL(this.gifData);
+			console.log(this.urlCreated);
 		});
 		// setInterval(() => {
 		// 	this.apiService.getRandomGif().then(result => {
